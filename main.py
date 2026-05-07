2dd7be989bf0e67be693bed62d3c293bebcbff2d
zjtheilen <zjtheilen@gmail.com>
2026-05-07 10:20:51 -0700

    create basic index.html, update gitignore, update main.py to avoid CORS issues

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, Optional
 from datetime import date
@@ -11,6 +12,14 @@ from models.entry import Entry
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 
 class EntryCreate(BaseModel):
