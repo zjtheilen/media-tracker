9ff44cc27ac650b9dc8a85b76e4f7a4cb8bb5167
zjtheilen <zjtheilen@gmail.com>
2026-05-12 09:16:49 -0700

    add scoring weight to rating categories

--- models/scoring_profile.py
+++ models/scoring_profile.py
@@ -1,38 +1,37 @@
 from models.scoring_category import ScoringCategory
 
-SCORING_PROFILES  = {
+SCORING_PROFILES = {
     "video": [
-        ScoringCategory("Writing"),
-        ScoringCategory("Pacing"),
-        ScoringCategory("Originality"),
-        ScoringCategory("Engagement"),
-        ScoringCategory("Thought Provoking"),
-        ScoringCategory("Emotional Impact"),
-        ScoringCategory("Sound"),
-        ScoringCategory("Acting"),
-        ScoringCategory("Cinematography"),
+        ScoringCategory("Writing", 1.25),
+        ScoringCategory("Pacing", 1.25),
+        ScoringCategory("Originality", 1.5),
+        ScoringCategory("Engagement", 1.5),
+        ScoringCategory("Thought Provoking", 1.5),
+        ScoringCategory("Emotional Impact", 1.5),
+        ScoringCategory("Sound", 1.0),
+        ScoringCategory("Acting", 0.9),
+        ScoringCategory("Cinematography", 1.0),
     ],
     "book": [
-        ScoringCategory("Writing"),
-        ScoringCategory("Pacing"),
-        ScoringCategory("Originality"),
-        ScoringCategory("Engagement"),
-        ScoringCategory("Thought Provoking"),
-        ScoringCategory("Emotional Impact"),
-        ScoringCategory("Setting"),
-        ScoringCategory("Curiosity"),
-        ScoringCategory("Formatting")
+        ScoringCategory("Writing", 1.5),
+        ScoringCategory("Pacing", 1.25),
+        ScoringCategory("Originality", 1.5),
+        ScoringCategory("Engagement", 1.5),
+        ScoringCategory("Thought Provoking", 1.5),
+        ScoringCategory("Emotional Impact", 1.5),
+        ScoringCategory("Setting", 1.0),
+        ScoringCategory("Curiosity", 1.4),
+        ScoringCategory("Formatting", 0.5),
     ],
     "game": [
-        ScoringCategory("Writing"),
-        ScoringCategory("Pacing"),
-        ScoringCategory("Originality"),
-        ScoringCategory("Engagement"),
-        ScoringCategory("Thought Provoking"),
-        ScoringCategory("Emotional Impact"),
-        ScoringCategory("Sound"),
-        ScoringCategory("Gameplay"),
-        ScoringCategory("Art")
-    ]
+        ScoringCategory("Writing", 1.1),
+        ScoringCategory("Pacing", 1.0),
+        ScoringCategory("Originality", 1.4),
+        ScoringCategory("Engagement", 1.5),
+        ScoringCategory("Thought Provoking", 1.25),
+        ScoringCategory("Emotional Impact", 1.4),
+        ScoringCategory("Sound", 1.0),
+        ScoringCategory("Gameplay", 1.5),
+        ScoringCategory("Art", 1.0),
+    ],
 }
-
