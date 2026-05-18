230613db7bd35cbb4eabc43e25878d45d2f348da
zjtheilen <zjtheilen@gmail.com>
2026-05-18 10:57:45 -0700

    finish adding testing for entries

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -140,7 +140,57 @@ def test_get_entry():
     assert data["title"] == "Silent Hill 2"
 
 def test_update_entry():
-    pass
+    payload = valid_game_payload()
+
+    create_response = client.post("/entries/", json=payload)
+
+    assert create_response.status_code == 200
+
+    created_entry = create_response.json()
+    entry_id = created_entry["id"]
+
+    updated_payload = valid_game_payload()
+    updated_payload["title"] = "Silent Hill 2 Remake"
+    updated_payload["notes"] = "Still peak psychological horror"
+
+    response = client.put(
+        f"/entries/{entry_id}",
+        json=updated_payload
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["message"] == "Entry updated"
+    assert data["entry_id"] == entry_id
+
+    get_response = client.get(f"/entries/{entry_id}")
+
+    updated_entry = get_response.json()
+
+    assert updated_entry["title"] == "Silent Hill 2 Remake"
+    assert updated_entry["notes"] == "Still peak psychological horror"
 
 def test_delete_entry():
-    pass
+    payload = valid_game_payload()
+
+    create_response = client.post("/entries/", json=payload)
+
+    assert create_response.status_code == 200
+
+    created_entry = create_response.json()
+    entry_id = created_entry["id"]
+
+    response = client.delete(f"/entries/{entry_id}")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["message"] == "Entry deleted"
+    assert data["entry_id"] == entry_id
+
+    get_response = client.get(f"/entries/{entry_id}")
+
+    assert get_response.status_code == 404
