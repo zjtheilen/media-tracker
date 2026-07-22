0a269c9ba091fbcd6dd530889f1a011e8d211025
zjtheilen <zjtheilen@gmail.com>
2026-07-22 01:54:30 -0700

    cleanup after backend migration

--- archive-engine/scoring-utils.js
+++
@@ -1,9 +0,0 @@
-function scoreThreshold(value, thresholds) {
-    for (const threshold of thresholds) {
-        if (value >= threshold.value) {
-            return threshold.score;
-        }
-    }
-
-    return 0;
-}
